d666fba042a3aaea854075425c6c91f975963aec
jeff john <jeffsams@gmail.com>
2017-06-13 12:27:40 -0400

    PlatformFall
    created a script to make the platforms fall if the player lands on them

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+
+    public float fallDelay = 1f;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"));
+            rb2d.isKinematic = false;
+        
+    }
+}
