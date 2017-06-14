072aa832577868cf31b53a02608f1e558e872e87
jeff john <jeffsams@gmail.com>
2017-06-13 23:10:54 -0400

    Final
    Added a Death Trigger

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -13,4 +13,13 @@ public class DeathTrigger : MonoBehaviour {
 	void Update () {
 		
 	}
+
+   void OnTriggerEnter2D(Collider2D other)
+    {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    
 }
